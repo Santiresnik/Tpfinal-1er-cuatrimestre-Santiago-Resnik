301ddf6e261bbe0f6d90b405955b6f18124f9e6a
SantiResnik <79484050+Santiresnik@users.noreply.github.com>
2022-06-03 10:47:41 -0300

    Rotation and jump
    Jump incomplete(not working)

--- Assets/Scripts/Salto.cs
+++ Assets/Scripts/Salto.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Salto : MonoBehaviour
 {
+    public float jumpForce;
+    public int maxJumps;
+    int hasJumps;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        hasJumps = maxJumps;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.Space) && hasJumps>0)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            hasJumps--;
+        }
         
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "ground")
+        {
+            hasJumps = maxJumps;
+        }
     }
 }
---
+++ Assets/Scripts/rotation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(0, 0, -rotationSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(0, 0, rotationSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-rotationSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(rotationSpeed, 0, 0);
+        }
+    }
+}
