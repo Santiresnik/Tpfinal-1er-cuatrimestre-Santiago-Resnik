56d2dd8c0b1f8a10667e97b8daa6e01c24788fbb
SantiResnik <79484050+Santiresnik@users.noreply.github.com>
2022-06-24 10:12:44 -0300

    time

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    public Text txtTimeFloored;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txtTimeFloored.text = Mathf.FloorToInt(Time.time).ToString();
+    }
+}
