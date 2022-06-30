e1e35a2fd621a9a23a7090341c24bb4229ccab64
SantiResnik <79484050+Santiresnik@users.noreply.github.com>
2022-06-30 09:02:26 -0300

    New platform

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -6,15 +6,17 @@ using UnityEngine.UI;
 public class TimeManager : MonoBehaviour
 {
     public Text txtTimeFloored;
+    public float reset;
     // Start is called before the first frame update
     void Start()
     {
-        
+        reset = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        txtTimeFloored.text = Mathf.FloorToInt(Time.time).ToString();
+        txtTimeFloored.text = Mathf.FloorToInt(Time.time - reset).ToString();
+
     }
 }
